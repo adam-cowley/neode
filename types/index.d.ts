13ca2cdf0b7b44d516d409f065dd85cb253ae1ca
Noumaan Shah <noumaan@smartfission.com>
2018-06-22 16:45:01 -0700

    Added schema type

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,6 +1,8 @@
 import {v1 as neo4j} from 'neo4j-driver';
 
 declare class Neode {
+  schema: Neode.Schema;
+
   /**
    * Constructor
    *
@@ -211,7 +213,7 @@ declare class Neode {
    * @param  {Int}                 skip
    * @return {Promise}
    */
-  all(label: string, properties: object, order: string | Array<any> | object, limit: number, skip: number): Promise<Neode.NodeCollection>;
+  all(label: string, properties?: object, order?: string | Array<any> | object, limit?: number, skip?: number): Promise<Neode.NodeCollection>;
 
   /**
    * Find a Node by it's label and primary key
@@ -268,7 +270,7 @@ declare namespace Neode {
 
   type PropertyType = string | number | boolean;
 
-  type PropertyTypes = 'uuid' | 'number' | 'string';
+  type PropertyTypes = 'uuid' | 'number' | 'string' | 'boolean';
 
   type Direction = 'direction_in' | 'direction_out' | 'direction_both' | 'in' | 'out';
 
@@ -287,6 +289,7 @@ declare namespace Neode {
       target:     string,
       relationship: string,
       required?:  boolean,
+      eager?:     boolean,
       default?:   any,
       direction:  Direction,
       properties?: {
@@ -505,7 +508,7 @@ declare namespace Neode {
      * @param  {Int}                 skip
      * @return {Promise}
      */
-    all(properties: Object, order: string | Array<any> | Object, limit: number, skip: number): Promise<NodeCollection>;
+    all(properties?: object, order?: string | Array<any> | object, limit?: number, skip?: number): Promise<NodeCollection>;
 
     /**
      * Find a Node by its Primary Key
@@ -630,6 +633,20 @@ declare namespace Neode {
      * @return {Array}
      */
     mergeFields(): Array<string>;
+  }
+
+  class Schema {
+
+    /**
+     * Neode will install the schema created by the constraints defined in your Node definitions.
+     */
+    install(): void;
+
+    /**
+     * Dropping the schema will remove all indexes and constraints created by Neode.
+     * All other indexes and constraints will be left intact.
+     */
+    drop(): void;
   }
 
   class RelationshipType {
@@ -875,7 +892,7 @@ declare namespace Neode {
      *
      * @return {Int}
      */
-    length(): number;
+    length: number;
 
     /**
      * Get a value by it's index
