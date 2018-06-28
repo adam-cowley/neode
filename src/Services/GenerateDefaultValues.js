13dc20e3d609ac0b9a87534de466d60f9840ee64
Noumaan Shah <noumaan@smartfission.com>
2018-06-28 11:34:57 -0700

    Added spatial properties

--- src/Services/GenerateDefaultValues.js
+++ src/Services/GenerateDefaultValues.js
@@ -1,4 +1,5 @@
 import uuid from 'uuid';
+import {v1 as neo4j} from 'neo4j-driver';
 import ValidationError from '../ValidationError';
 
 /**
@@ -27,6 +28,58 @@ function CleanValue(config, value) {
 
         case 'timestamp':
             value = value instanceof Date ? value.getTime() : value;
+            break;
+
+        case 'DateTime':
+            value = value instanceof Date ?
+                new neo4j.types.DateTime(
+                    value.getFullYear(),
+                    value.getMonth() + 1,
+                    value.getDate(),
+                    value.getHours(),
+                    value.getMinutes(),
+                    value.getSeconds(),
+                    value.getMilliseconds() * 1000000,  // nanoseconds
+                    value.getTimezoneOffset() * 60      // seconds
+                ) : value;
+            break;
+
+        case 'Point':
+            // SRID values: @https://neo4j.com/docs/developer-manual/current/cypher/functions/spatial/
+            if (value.longitude) { // WGS 84
+                if (isNaN(value.height)) {
+                    value = new neo4j.types.Point(
+                        4326, // WGS 84 2D
+                        value.longitude,
+                        value.latitude
+                    );
+                }
+                else {
+                    value = new neo4j.types.Point(
+                        4979, // WGS 84 3D
+                        value.longitude,
+                        value.latitude,
+                        value.height
+                    );
+                }
+            }
+            else {
+                if (isNaN(value.z)) {
+                    value = new neo4j.types.Point(
+                        7203, // Cartesian 2D
+                        value.x,
+                        value.y
+                    );
+                }
+                else {
+                    value = new neo4j.types.Point(
+                        9157, // Cartesian 3D
+                        value.x,
+                        value.y,
+                        value.z
+                    );
+                }
+            }
             break;
     }
 
